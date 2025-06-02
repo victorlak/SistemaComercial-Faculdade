f0402d72a691e9b6192c740aa67e30e9b5798d4f
WHENDELL BRITO OLIVEIRA <07767770680@campusrp-lab.riopomba.ifsudestemg.edu.br>
2025-06-02 10:06:44 -0300

    fix

--- barber_app/src/services/firebaseConfig.js
+++ barber_app/src/services/firebaseConfig.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCYQqF8QqVVgqfx50eqtv5dfc_olEICatc",
   authDomain: "projeto-barbearia-92c43.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-XEJLN13BEV"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
