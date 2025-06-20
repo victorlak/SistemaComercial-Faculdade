e564cf30a6d81eba54d787f970f8e65a8c50c3c1
Mylena Antonelli <mylena32@icloud.com>
2025-06-20 17:19:16 -0300

    #dev_01 #39

--- barber_app/src/components/Button/index.tsx
+++ barber_app/src/components/Button/index.tsx
@@ -1,14 +1,16 @@
-import { TouchableOpacity, Text, TouchableOpacityProps } from "react-native";
+import { TextStyle, ViewStyle, TouchableOpacity, Text, TouchableOpacityProps } from "react-native";
 import { styles } from './styles';
 
-interface ButtonProps extends TouchableOpacityProps {
-  label?: string;
+type Props = TouchableOpacityProps & {
+    label: string;
+    style?: ViewStyle;
+    textStyle?: TextStyle;
 }
 
-export default function Button({ label, ...rest }: ButtonProps) {
+export default function Button({ label, style, textStyle, ...rest }: Props) {
   return (
-    <TouchableOpacity activeOpacity={0.8} style={styles.button} {...rest}>
-      {label && <Text style={styles.title}>{label}</Text>}
+    <TouchableOpacity activeOpacity={0.8} style={[styles.button, style]} {...rest}>
+      <Text style={[styles.title, textStyle]}>{label}</Text>
     </TouchableOpacity>
   );
 }
